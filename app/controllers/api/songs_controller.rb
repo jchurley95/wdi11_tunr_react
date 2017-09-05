f88b7e51708fc9d992878e2334ed078f568627c7
king0120 <king0120@gmail.com>
2017-09-05 12:29:22 -0400

    added Procfile

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -5,12 +5,12 @@ class Api::SongsController < ApplicationController
 
     render json: @songs
   end
-  
+
   def show
     @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.find params[:id]
 
     render json: @song
   end
-  
+
 end
