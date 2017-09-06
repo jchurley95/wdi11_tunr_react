8088ce890ec1401ecf5e2f0545c1228a80f8410d
jchurley95 <jchurley95@gmail.com>
2017-09-06 08:29:33 -0400

    Updated NewArtist component

--- client/src/components/NewArtist.js
+++ client/src/components/NewArtist.js
@@ -1,58 +1,41 @@
-import React, { Component } from 'react';
-import { Redirect, Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from 'react'
 
 class NewArtist extends Component {
- constructor(){
-   super();
-   this.state = {
-       name: '',
-       picture_url: '',
-       nationality: '',
-
-       redirect: false
-   }
- }
- _submitArtist = async (e) => {
-    e.preventDefault();
-    const payload = {
-      name: this.state.name,
-      nationality: this.state.nationality,
+    constructor(){
+        super()
+        this.state = {
+        newArtist: {
+            name: "",
+            nationality: "",
+            photo_url: ""
+        }
+        }
     }
-    const response = await axios.post('/api/artists');
-    this.setState({redirect: true})
-  }
- _handleChange = (e) => {
-   const newState = {...this.state};
-   newState[e.target.name] = e.target.value;
-   this.setState(newState);
- }
+    _handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addArtist(this.state.newArtist)
+    }
+    _handleChange = (e) => {
+        const attributeValue = e.target.value
+        const attributeName = e.target.name
+        const newState = {...this.state}
+        newState.newArtist[attributeName] = attributeValue
+        this.setState(newState)
+    }
 
- render() {
-   if (this.state.redirect){
-     return <Redirect to="/" />
-   }
-   return (
-     <div>
-       <form onSubmit={this._signIn}>
-         <div>
-           <label htmlFor="artist">E-mail: </label>
-           <input onChange={this._handleChange} type="text" name="name" value={this.state.name} />
-         </div>
-         <div>
-           <label htmlFor="picture_url">Picture URL: </label>
-           <input onChange={this._handleChange} type="text" name="picture_url" value={this.state.picture_url} />
-         </div>
-         <div>
-           <label htmlFor="nationality">Password: </label>
-           <input onChange={this._handleChange} type="text" name="nationality" value={this.state.nationality} />
-         </div>
-         <button onClick={this._submitArtist}>Create New Artist</button>
-         <Link to="/">Home</Link>
-       </form>
-     </div>
-   );
- }
+    render () {
+        return (
+            <form onSubmit={this._handleSubmit}>
+                <label>Artist Name:</label>
+                    <input type="text" name="name" placeholder="Artist Name" onChange={this._handleChange}/>
+                <label>Artist Nationality:</label>
+                    <input type="text" name="nationality" placeholder="Nationality" onChange={this._handleChange}/>
+                <label>Artist Banner Photo URL:</label>
+                    <input type="text" name="photo_url" placeholder="Artist Banner Photo URL" onChange={this._handleChange}/>
+                <input type="submit" value="Add Artist"/>
+            </form>
+        )
+    }
 }
 
 export default NewArtist;
